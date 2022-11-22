62cdf3a1d7213f767dc02490ea455d267ef23bed
omargtdev <omar.gutierrez.tafur@gmail.com>
2022-11-22 17:28:28 -0500

    EP3 Urls backend fixed

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,8 +1,5 @@
 export default class API {
     
-/*     public static baseUrl: string = "https://somenodebackend-production.up.railway.app"
-    public static baseUrlTechbiz: string = "https://somenodebackend-production.up.railway.app/techbiz" */
-    public static baseUrl: string = "http://localhost:3002"
-    public static baseUrlTechbiz: string = `${this.baseUrl}/techbiz`
-
+    public static baseUrl: string = "https://somenodebackend-production.up.railway.app"
+    public static baseUrlTechbiz: string = "https://somenodebackend-production.up.railway.app/techbiz"
 }
